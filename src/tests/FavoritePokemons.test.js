d779a4e5f6ed2c427aba411bbfe40d6b0ae79f25
Teresa Marques <taotaodoce@gmail.com>
2022-09-19 15:38:55 -0300

    requisito 3 concluído

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testes do requisito 3', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found,'
+ + 'caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const title = screen.getByText('No favorite pokemon found');
+    expect(title).toBeInTheDocument();
+  });
+});
