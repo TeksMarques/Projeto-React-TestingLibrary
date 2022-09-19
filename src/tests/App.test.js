cda7696ce4f07f96e0edaf654d2b075b6bf6cb05
Teresa Marques <taotaodoce@gmail.com>
2022-09-19 15:10:17 -0300

    requisito 1 concluido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do requisito 1', () => {
+  test('Teste se o topo da aplicação contém um conjunto fixo de links', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavorite = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
